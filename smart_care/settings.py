0084c25ecdb2ef4b49d8a999e6136b4acf658305
MDAsrafulhasan <soumick940@gmail.com>
2024-08-31 09:27:38 +0600

    updated

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'smart_care.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',   # eta use kore sob kichuke authenticatw kora jay
+    # ]
 }
 
 
